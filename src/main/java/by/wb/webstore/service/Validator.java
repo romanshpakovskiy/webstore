a69a3a64928563ae51924de50819827dd3cc519d
Roman <romanshpakovskiy15@gmail.com>
2020-11-05 21:05:20 +0300

    added email and password Validator

---
+++ src/main/java/by/wb/webstore/service/Validator.java
@@ -0,0 +1,15 @@
+package by.wb.webstore.service;
+
+public class Validator {
+    private static final String EMAIL_PATTERN = "[[a-z0-9]+@(?!-)[A-Za-z0-9-]{1,63}!-)\\\\.)]+[A-Za-z]{2,6}$";
+    private static final String PASSWORD_PATTERN ="[A-Za-z]+[0-9]+{8,16}$";
+
+    public boolean checkEmailValidation(String email) {
+        return email.matches(EMAIL_PATTERN) && email.isEmpty();
+    }
+
+    public boolean checkPasswordValidation(String password) {
+        return password.isEmpty() && password.matches(PASSWORD_PATTERN) ;
+    }
+
+}
