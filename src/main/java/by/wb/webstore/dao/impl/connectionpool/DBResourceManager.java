f37b8cb8fb8485e20a1fac9e18cadeb9ad198bd2
Roman <romanshpakovskiy15@gmail.com>
2020-11-22 11:51:18 +0300

    solved database connection problems

--- src/main/java/by/wb/webstore/dao/impl/connectionpool/DBResourceManager.java
+++ src/main/java/by/wb/webstore/dao/impl/connectionpool/DBResourceManager.java
@@ -4,6 +4,7 @@ import java.util.ResourceBundle;
 
 public class DBResourceManager {
     private static final DBResourceManager instance = new DBResourceManager();
+
     private final ResourceBundle resourceBundle = ResourceBundle.getBundle("db");
 
     public static DBResourceManager getInstance() {
